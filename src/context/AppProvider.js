174e5f673f7f9baf9db599983f4643081186680b
Gustavo Anselmo <gugaasp18@gmail.com>
2022-11-30 18:30:23 -0300

    Requisitos 57 a 60 realizados

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,16 +1,37 @@
-// import { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useMemo } from 'react';
+import Proptypes from 'prop-types';
 import AppContext from './AppContext';
 
-function AppProvider({ children }) {
+export default function Provider({ children }) {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [passoword, setPassoword] = useState('');
+  const [button, setButton] = useState(true);
+
+  // Link para a explicação do useMemo
+  // https://beta.reactjs.org/apis/react/useMemo
+
+  const context = useMemo(() => ({
+    loginEmail,
+    setLoginEmail,
+    passoword,
+    setPassoword,
+    button,
+    setButton,
+  }), [
+    loginEmail,
+    setLoginEmail,
+    passoword,
+    setPassoword,
+    button,
+    setButton,
+  ]);
+
   return (
-    <AppContext.Provider>
+    <AppContext.Provider value={ context }>
       {children}
     </AppContext.Provider>
   );
 }
-
-AppProvider.propTypes = {
-  children: PropTypes.node.isRequired,
+Provider.propTypes = {
+  children: Proptypes.node.isRequired,
 };
-export default AppProvider;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,49 @@
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+
+  function getEmail() {
+    if (localStorage.getItem('user')) {
+      const { email } = JSON.parse(localStorage.getItem('user'));
+      return email;
+    }
+    return '';
+  }
+  function pushLogin() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
-    <Header title="Profile" isSearchOn={ false } />
+    <div>
+      <Header title="Profile" isSearchOn={ false } />
+      <form>
+        <p data-testid="profile-email">{ getEmail() }</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ pushLogin }
+        >
+          Logout
+        </button>
+      </form>
+    </div>
   );
 }
 
