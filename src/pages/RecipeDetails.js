ee10821470d7964f5e955d0bcb9ff4181188c948
Thais-Gonzaga <devthaisgonza@gmail.com>
2022-12-01 11:02:43 -0300

    feat: fazendo a requisição da API comidas e bebidas, guardando no estado
     ref: #requisito 24

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchDrinkId, fetchMealsId } from '../services/fetchApi';
+
+// const idMeal = '52882';
+// const idDrink = '17256';
+
 function RecipeDetails() {
+  const { id } = useParams();
+  const [responseDrinkId, setResponseDrinkId] = useState([]);
+  const [responseMealId, setResponseMealId] = useState([]);
+
+  useEffect(() => {
+    const fetchApiD = async () => {
+      const drinks = await fetchDrinkId(id);
+      if (!drinks) return;
+      setResponseDrinkId(drinks);
+    };
+    fetchApiD();
+  }, []);
+
+  useEffect(() => {
+    const fetchApiM = async () => {
+      const meals = await fetchMealsId(id);
+      if (!meals) return;
+      setResponseMealId(meals);
+    };
+    fetchApiM();
+  }, []);
+
+  console.log(responseDrinkId);
+  console.log(responseMealId);
+
   return (
-    <div>Recipe Details</div>
+    <h1>Recipe Details</h1>
   );
 }
 
