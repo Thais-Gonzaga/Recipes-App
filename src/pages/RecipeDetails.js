4cbf3533eb1c3ad4dd593b521d25e9678b408c9c
Thais-Gonzaga <devthaisgonza@gmail.com>
2022-12-01 18:33:05 -0300

    feat: refatorando o componente RecipeDetails e renderizando os detalhes
     ref: #requisito25

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,104 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { fetchDrinkId, fetchMealsId } from '../services/fetchApi';
-
-// const idMeal = '52882';
-// const idDrink = '17256';
+import valuesApi from '../services/valuesApi';
 
 function RecipeDetails() {
   const { id } = useParams();
-  const [responseDrinkId, setResponseDrinkId] = useState([]);
-  const [objDrink, setObjDrink] = useState({});
-  const [responseMealId, setResponseMealId] = useState([]);
-  const [objMeal, setObjMeal] = useState({});
+  const [response, setResponse] = useState({});
+  const { location: { pathname } } = useHistory();
+  const isDrink = pathname.includes('drinks');
+  const fetchApi = isDrink ? fetchDrinkId : fetchMealsId;
+  const ingredients = valuesApi(response, 'strIngredient');
+  const measure = valuesApi(response, 'strMeasure');
+
+  const fetchCB = useCallback(async () => {
+    const data = await fetchApi(id);
+    if (!data) return;
+    setResponse(data[0]);
+  }, [id, fetchApi]);
 
   useEffect(() => {
-    const fetchApiD = async () => {
-      const drinks = await fetchDrinkId(id);
-      if (!drinks) return;
-      setResponseDrinkId(drinks);
-      setObjDrink(drinks[0]);
-    };
-    fetchApiD();
-  }, []);
+    fetchCB();
+  }, [fetchCB]);
 
-  useEffect(() => {
-    const fetchApiM = async () => {
-      const meals = await fetchMealsId(id);
-      if (!meals) return;
-      setResponseMealId(meals);
-      setObjMeal(meals[0]);
-    };
-    fetchApiM();
-  }, []);
+  // const ingredientsDrinks = Object.entries(response)
+  //   .reduce((acc, [key, value]) => (
+  //     value && key.includes('strIngredient') ? [...acc, { ing: value }] : acc), []);
 
-  const ingredientsDrinks = Object.entries(objDrink)
-    .reduce((acc, [key, value]) => (
-      value && key.includes('strIngredient') ? [...acc, { ing: value }] : acc), []);
+  // const measureDrinks = Object.entries(response)
+  //   .reduce((acc, [key, value]) => (
+  //     value && key.includes('strMeasure') ? [...acc, { meas: value }] : acc), []);
 
-  const ingredientsMeals = Object.entries(objMeal)
-    .reduce((acc, [key, value]) => (
-      value && key.includes('strIngredient') ? [...acc, { ing: value }] : acc), []);
-  const { strDrinkThumb, strDrink, strAlcoholic, strInstructions } = objDrink;
+  const { strDrinkThumb, strDrink, strAlcoholic, strInstructions, strMealThumb,
+    strMeal, strCategory, strYoutube } = response;
 
   return (
     <div>
       <h1>Recipe Details</h1>
       <div>
-        {responseDrinkId.length
-        && (
-          <section>
-            <img
-              data-testid="recipe-photo"
-              src={ strDrinkThumb }
-              alt={ strDrink }
-            />
-            <h2 data-testid="recipe-title">{ strDrink }</h2>
-            <h3> Category:</h3>
-            <p data-testid="recipe-category">{ strAlcoholic }</p>
-            <h3> Ingredients:</h3>
-            <ul>
-              {ingredientsDrinks.map(({ ing }, index) => (
-                <li
-                  data-testid={ `${index}-ingredient-name-and-measure` }
-                  key={ index }
-                >
-                  {`${ing} ${measureDrinks[index]}`}
-                </li>
-              ))}
-            </ul>
-            <h3>Instructions</h3>
-            <p data-testid="instructions">{strInstructions}</p>
+        <section>
+          <img
+            width={ 250 }
+            data-testid="recipe-photo"
+            src={ strDrinkThumb || strMealThumb }
+            alt={ strDrink || strMeal }
+          />
+          <h2 data-testid="recipe-title">{ strDrink || strMeal}</h2>
+          <h3> Category:</h3>
+          <p data-testid="recipe-category">{ strAlcoholic || strCategory }</p>
+          <h3> Ingredients:</h3>
+          <ul>
+            {ingredients.map(({ k }, index) => (
+              <li
+                data-testid={ `${index}-ingredient-name-and-measure` }
+                key={ index }
+              >
+                {`${k} ${measure[index].k}`}
+              </li>
+            ))}
+          </ul>
+          <h3>Instructions</h3>
+          <p data-testid="instructions">{strInstructions}</p>
 
-          </section>
-        )}
-        {responseMealId.length
-        && (
-          <section>
-            {/* <img
-              data-testid="recipe-photo"
-              src={ strDrinkThumb }
-              alt={ strDrink }
-            />
-            <h2 data-testid="recipe-title">{ strDrink }</h2>
-            <h3> Category:</h3>
-            <p data-testid="recipe-category">{ strAlcoholic }</p>
-            <h3> Ingredients:</h3>
-            <ul>
-              {ingredientsDrinks.map(({ ing }, index) => (
-                <li
-                  data-testid={ `${index}-ingredient-name-and-measure` }
-                  key={ index }
-                >
-                  {`${ing} ${measureDrinks[index]}`}
-                </li>
-              ))}
-            </ul>
-            <h3>Instructions</h3>
-            <p data-testid="instructions">{strInstructions}</p> */}
-
-          </section>
-        )}
+          { !isDrink
+              && <iframe
+                data-testid="video"
+                src={ strYoutube && strYoutube.replace(/watch\?v=/g, 'embed/') }
+                title={ strMeal }
+              />}
+        </section>
       </div>
     </div>
   );
