4cb1e85e6ba162249812bfaee5e36461cbb0792e
Thais-Gonzaga <devthaisgonzaga@gmail.com>
2022-12-07 13:04:41 -0300

    feat: criando função de remover item do localStorage
     ref:#requisitos 35 a 36

--- src/services/localStore.js
+++ src/services/localStore.js
@@ -1,8 +1,16 @@
-const localStoreItem = (key, item) => {
-  const current = JSON.parse(localStorage.getItem(key)) || [];
+export const getLocalStore = (key) => JSON.parse(localStorage.getItem(key));
+
+export const add = (key, item) => {
+  // console.log(item);
+  const current = getLocalStore(key) || [];
   const newItem = [...current, item[0]];
   const data = JSON.stringify(newItem);
   localStorage.setItem(key, data);
 };
 
-export default localStoreItem;
+export const remove = (key, idSelect) => {
+  const current = getLocalStore(key) || [];
+  const newItem = current.filter(({ id }) => id !== idSelect);
+  const data = JSON.stringify(newItem);
+  localStorage.setItem(key, data);
+};
