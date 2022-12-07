4b284cbf76912e14aae8fd4c2149d94f01034866
Thais-Gonzaga <devthaisgonzaga@gmail.com>
2022-12-07 17:24:57 -0300

    resolvendo conflitos

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,10 +1,33 @@
-// import { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
 function AppProvider({ children }) {
+  const [loginEmail, setLoginEmail] = useState('');
+  const [passoword, setPassoword] = useState('');
+  const [button, setButton] = useState(true);
+
+  // Link para a explicação do useMemo
+  // https://beta.reactjs.org/apis/react/useMemo
+
+  const context = useMemo(() => ({
+    loginEmail,
+    setLoginEmail,
+    passoword,
+    setPassoword,
+    button,
+    setButton,
+  }), [
+    loginEmail,
+    setLoginEmail,
+    passoword,
+    setPassoword,
+    button,
+    setButton,
+  ]);
+
   return (
-    <AppContext.Provider>
+    <AppContext.Provider value={ context }>
       {children}
     </AppContext.Provider>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,76 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+
 function Login() {
+  const {
+    loginEmail,
+    button,
+    setLoginEmail,
+    passoword,
+    setPassoword,
+    setButton,
+  } = useContext(AppContext);
+
+  const fetchButton = () => {
+    const format = { email: loginEmail };
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify(format));
+  };
+
+  const testLogin = () => {
+    const testEmail = loginEmail.includes('@' && '.com');
+    const max = 6;
+
+    if (passoword.length >= max && testEmail) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    const { id, value } = target;
+    if (id === 'email-input') {
+      setLoginEmail(value);
+    } else {
+      setPassoword(value);
+    }
+    testLogin();
+  };
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <label htmlFor="email-input">
+          <input
+            type="text"
+            id="email-input"
+            placeholder="Email"
+            data-testid="email-input"
+            value={ loginEmail }
+            onChange={ (element) => handleChange(element) }
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            type="text"
+            id="password-input"
+            placeholder="Password"
+            data-testid="password-input"
+            value={ passoword }
+            onChange={ (element) => handleChange(element) }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ button }
+          onClick={ () => fetchButton() }
+        >
+          Enter
+        </button>
+      </form>
+    </div>
   );
 }
 
