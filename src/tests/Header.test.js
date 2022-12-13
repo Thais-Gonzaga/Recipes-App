e11efa33143da4a05256558a00519f81c694e494
WILLIAM <williamasjr@gmail.com>
2022-12-13 19:11:54 -0300

    group-18-req 40

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -7,6 +7,7 @@ import renderWithRouter from '../renderWithRouter';
 
 // import App from '../App';
 import Header from '../components/Header';
+import App from '../App';
 // import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 
 describe('Header page tests', () => {
@@ -17,19 +18,11 @@ describe('Header page tests', () => {
     expect(profileImage).toHaveAttribute('alt', 'profile');
     expect(profileImage).toBeInTheDocument();
   });
-  test('tests if the Header page contains a search icon', () => {
-    renderWithRouter(<Header title="title" />);
-    const profileImage = (screen.getAllByRole('img'))[1];
-    expect(profileImage).toHaveAttribute('src', 'searchIcon.svg');
-    expect(profileImage).toHaveAttribute('alt', 'search');
-    expect(profileImage).toBeInTheDocument();
-  });
-  test('tests if the when clicking the profile icon, the user is redirected to the profile page', () => {
-    const { history } = renderWithRouter(<Header title="title" />);
+  it('verifique se a pagina Footer tem as imagens', () => {
+    renderWithRouter(<App />);
+    const imgDrink = screen.getByTestId('recipe-photo');
+    // const imgMeals = screen.getByTestId('meals-bottom-btn');
 
-    const linkProfile = (screen.getAllByRole('button')[0]);
-    userEvent.click(linkProfile);
-    const { location: { pathname } } = history;
-    expect(pathname).toBe('/profile');
+    expect(imgDrink).toBeInTheDocument();
   });
 });
